90c40b6cf38f1fcd5c576ac39414058d530d1eff
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-27 13:47:36 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,8 +5,11 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    this.state = {
+      color:this.color
+    };
   }
 
   genRow = (vals) => (
